5e20ae8b420e847a6ac89b83c20f531e2494751c
keith <8584184+git-kv@users.noreply.github.com>
2024-04-05 23:03:58 -0500

    working on timer, disable buttons when timer running

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var startTimerBtn = document.getElementById("start-timer-btn");
+var resetBtn = document.getElementById("reset-btn");
+let sets = 0;
+
 startTimerBtn.addEventListener('click', startTimer);
+resetBtn.addEventListener('click', resetSetCount);
 
 function startTimer() {
   document.getElementById("start-timer-btn").disabled = true;
+  document.getElementById("reset-btn").disabled = true;
   let counter = document.getElementById("rest-time").value;
   console.log(counter);
 
@@ -13,7 +18,15 @@ function startTimer() {
 
     if (counter < 0) {
       clearInterval(interval);
+      sets++;
+      document.getElementById("sets").innerHTML = sets;
       document.getElementById("start-timer-btn").disabled = false;
+      document.getElementById("reset-btn").disabled = false;
     }
   }, 1000);
+}
+
+function resetSetCount() {
+  sets = 0;
+  document.getElementById("sets").innerHTML = sets;
 }
